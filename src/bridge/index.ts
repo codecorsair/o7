5e4c3ee06baf19042b9442ce349531ba099ddcde
Ralph Schuler <ralph.schuler@nyphon.de>
2023-03-23 18:17:10 +0100

    fix config usage

--- src/bridge/index.ts
+++ src/bridge/index.ts
@@ -1,16 +1,17 @@
 import { Bridge } from 'discord-cross-hosting';
 import { RatelimitManager } from 'discord-cross-ratelimit';
 import { createLogger } from '@/shared/utils/logger';
+import Config from './Config';
 
 const logger = createLogger();
 
 const server = new Bridge({
-  port: 4444, // The Port of the Server | Proxy Connection (Replit) needs Port 443
-  authToken: 'Your_auth_token_same_in_cluster.js',
-  totalShards: 40, // The Total Shards of the Bot or 'auto'
-  totalMachines: 2, // The Total Machines, where the Clusters will run
-  shardsPerCluster: 10, // The amount of Internal Shards, which are in one Cluster
-  token: 'Your_Bot_Token'
+  port: Config.port, // The Port of the Server | Proxy Connection (Replit) needs Port 443
+  authToken: Config.authToken,
+  totalShards: Config.totalShards, // The Total Shards of the Bot or 'auto'
+  totalMachines: Config.totalMachines, // The Total Machines, where the Clusters will run
+  shardsPerCluster: Config.shardsPerCluster, // The amount of Internal Shards, which are in one Cluster
+  token: Config.token
 });
 new RatelimitManager(server);
 server.on('debug', (message) => logger.debug(message));
